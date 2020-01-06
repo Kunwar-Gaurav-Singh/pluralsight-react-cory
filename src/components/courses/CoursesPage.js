4554ef54e6794d48ae2e77bf30dbb007975a0c04
Kunwar gaurav singh <mail.kunwargaurav@gmail.com>
2020-01-07 00:41:47 +0530

    all files

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseAction";
+import PropTypes from "prop-types";
+
+class CoursesPage extends React.Component {
+    state = {
+        course: {
+            title: ""
+        }
+    };
+
+    handleChange = event => {
+        const course = { ...this.state, title: event.target.value };
+        this.setState({ course });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>Courses</h2>
+                <h3>Add course</h3>
+                <input type="text" onChange={this.handleChange} value={this.state.course.title} />
+                <input type="submit" value="Save" />
+                {this.props.courses.map(course => (
+                    <div key={course.title}>{course.title}</div>
+                ))}
+            </form>
+        )
+    }
+}
+
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    dispatch: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps)(CoursesPage);
